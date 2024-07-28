7171878592218926ea1eee2888aae95d20c3d795
jaypatel342005 <pateljay97378@gmail.com>
2024-07-28 16:58:43 +0530

    Java

---
+++ Lab 4/Lab4_8.java
@@ -0,0 +1,22 @@
+/**
+ * Lab4_8
+ */
+
+ public class Lab4_8 {
+    public static void main(String[] args) {
+        int n = 3;
+        for(int i = 1 ; i <= n ; i++) {
+            for(int j = 1 ; j <=i ; j++) {
+                   System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i = n - 1 ; i >= 1 ; i--){
+            for(int j = 1 ; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+     }
+    
+}
